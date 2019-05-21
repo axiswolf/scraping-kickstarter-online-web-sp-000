46ee5a1ac31a491aa293d08bc904ece16e09aa5e
Karen V <karenvo@usc.edu>
2019-05-21 02:42:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,14 +1,14 @@
 # require libraries/modules here
 require 'nokogiri'
 require 'pry'
-class KickStarter
+# class KickStarter
   def create_project_hash
     # write your code here
     # This just opens a file and reads it into a variable
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
   end
-end
+# end
     binding.pry
     create_project_hash
 #Name and Author
