b2b3ea71b18942129ef6ec5e20dc9ac2e17b13e2
Karen V <karenvo@usc.edu>
2019-05-21 02:33:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,17 @@
 # require libraries/modules here
-
+require 'nokogiri'
+require 'pry'
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 end
+  binding.pry
+
+#Name and Author
+#create_project_hash.css(".project").first.css("h2").text
+#Description
+#create_project_hash.css(".project").first.css("p").text
+#Image Link
+#Location
+#Percent Funded
